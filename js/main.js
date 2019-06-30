2dd958fc4f487028438fa61854a70c508a028752
incjm <46875428+incjm@users.noreply.github.com>
2019-06-30 20:58:36 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-//$(document).ready(function (){
-    //$("#click").click(function (){
-       // $('html, body').animate({
-        //    scrollTop: $("#home").offset().top
-       // }, 2300);
-    //});
-//});
+$(document).ready(function (){
+    $("#click").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#home").offset().top
+        }, 2300);
+    });
+});
 
-//$(document).ready(function(){
-  //  $(window).scroll(function(){
-   //   if($(window).scrollTop()>120){
-    //    $('#click').fadeIn(900)
-    //  }else{
-    //    $('#click').fadeOut(100)
-   //   }
-   //});
-//  });
+$(document).ready(function(){
+    $(window).scroll(function(){
+      if($(window).scrollTop()>120){
+        $('#click').fadeIn(900)
+      }else{
+        $('#click').fadeOut(100)
+      }
+   });
+  });
 
   //  let animeEnjoy = document.getElementById('enjoy');
   //  let animeEvery = document.getElementById('every');
@@ -49,13 +49,12 @@
 
   // animeEnjoy.style.color = animation(anime);
   // setInterval(animeEnjoy.style.color = animation(), 1000);
-  window.onload=function(){
-    var image = document.getElementById("enjoy");
-    var i = 0, colors = ['red', 'green', 'blue', 'yellow', 'orange'],t = [700,300]; //2 сек есть картинка, 5 сек нет
-    function show() {
-      let colorIndex = Math.floor(Math.random()*4);
-    image.style.color = colors[colorIndex];
-    setInterval(show,t[colorIndex], 1000);
-    }
-    show()
-    }
+  function flashColor() {
+    var elem=document.all.enjoy.style;
+if (elem.color=="deepskyblue")
+    elem.color="midnightblue"
+else
+    elem.color="deepskyblue"
+}
+
+setInterval(flashColor, 1000)
