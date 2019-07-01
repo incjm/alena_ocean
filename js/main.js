4e58bdae8c6a307e4be2de19bc6c749bb137e53e
incjm <46875428+incjm@users.noreply.github.com>
2019-07-01 23:47:20 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,54 +1,27 @@
-$(document).ready(function (){
-    $("#click").click(function (){
-        $('html, body').animate({
-            scrollTop: $("#home").offset().top
-        }, 2300);
-    });
-});
+var arrow = document.getElementById("click");
+window.onscroll = function() {
+  if (document.body.scrollTop > document.documentElement.clientHeight) {
+    arrow.style.display = "none";
+  } else {
+    arrow.style.display = "block";
+  }
+};
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-      if($(window).scrollTop()>120){
-        $('#click').fadeIn(900)
-      }else{
-        $('#click').fadeOut(100)
-      }
-   });
-  });
+var timeOut;
+function scrolUp() {
+  var top = Math.max(
+    document.body.scrollTop,
+    document.documentElement.scrollTop
+  );
+  if (top > 0) {
+    window.scrollBy(0, -5);
+    timeOut = setTimeout("scrolUp()", 10);
+  } else {
+    clearTimeout(timeOut);
+    arrow.style.display = "none";
+  }
+}
 
-  //  let animeEnjoy = document.getElementById('enjoy');
-  //  let animeEvery = document.getElementById('every');
-  // animeEvery.style.color="black";
-  // let animeMoment = document.getElementById('moment');
-
-  
-  // function animation() {
-  // let colors = ['red', 'green', 'blue', 'yellow', 'orange'];
-  // let colorIndex = Math.floor(Math.random()*4);
-  // return colors[colorIndex];
-  // };
-
-  // let anime = setInterval(animation(), 1000);
-  // setInterval(anime, 1000);
-  // function loop() {
-  //  return setInterval(animation(), 1000);
-  // };
-    // setInterval (document.querySelector('#enjoy').style['color'] = animation(), 1000);
-    // console.log(setInterval(animation()), 1000);
-  // };
-  // function styl() {
-    // while (true) {
-  // document.querySelector('#enjoy').style['color'] = animation();
-    
-      // }
-  // };
-  // animeEvery.style.color = "red";
-  // animation()
-  // animeEnjoy.style.color = "red";   //setInterval(animation(), 1000);
-  // (animeEnjoy. animeEvery. animeMoment).classList.add("enjoyEveryMoment");
-
-  // animeEnjoy.style.color = animation(anime);
-  // setInterval(animeEnjoy.style.color = animation(), 1000);
   function flashColor() {
     var elem=document.all.enjoy.style;
 if (elem.color=="cyan")
